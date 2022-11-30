a371b189d277fee772b86abcfa50d8cd5ecb6ec0
Lee Yi Xuan <s10243281@connect.np.edu.sg>
2022-11-30 09:20:10 +0800

    update

--- home/home.js
+++ home/home.js
@@ -6,12 +6,12 @@ function myFunction() {
       x.className = "topnav";
     }
   }
-
   function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
     } else {
-      x.style.display = "block";
+      x.className = "topnav";
     }
   }
+  
